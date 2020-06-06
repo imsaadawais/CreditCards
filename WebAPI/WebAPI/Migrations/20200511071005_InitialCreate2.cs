83fed12ae5edb5f5374949733efd1dc7682d5e41
Saad Awais <m_saad_awais@hotmail.com>
2020-06-05 18:42:40 -0700

    1st commit

---
+++ WebAPI/WebAPI/Migrations/20200511071005_InitialCreate2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class InitialCreate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CardMumber",
+                table: "PaymentDetails",
+                newName: "CardNumber");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CardNumber",
+                table: "PaymentDetails",
+                newName: "CardMumber");
+        }
+    }
+}
